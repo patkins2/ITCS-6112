55a9afb7fb4193cffd1bdaf748cf791e6e3603b1
Xinyao Yi <yxyict@gmail.com>
2020-02-23 19:15:42 -0500

    test

--- AngularJsSqlEx/src/main/java/com/jcg/java/MyDb.java
+++ AngularJsSqlEx/src/main/java/com/jcg/java/MyDb.java
@@ -19,7 +19,7 @@ public class MyDb {
 		String connUrl = "jdbc:mysql://localhost:3306/test?";
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connObj = DriverManager.getConnection(connUrl + "user=root&password=root");			
+			connObj = DriverManager.getConnection(connUrl + "user=root&password=123456");			
 		} catch (Exception exObj) {
 			exObj.printStackTrace();
 		}
