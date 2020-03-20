4f0083421047224154db6cd93622da89b7c35297
patkins2 <patkins2@uncc.edu>
2020-03-19 20:19:57 -0400

    created database and server

---
+++ src/MyDb.java
@@ -0,0 +1,41 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class MyDb {
+
+	static ResultSet rsObj = null;
+	static Statement stmtObj = null;
+	static Connection connObj = null;
+
+	/***** Method #1 :: This Method Is Used To Create A Connection With The Database *****/
+	private static Connection connectDb() {
+		String connUrl = "jdbc:mysql://localhost:3306/itcs6112_project?";
+		try {
+				Class.forName("com.mysql.jdbc.Driver");
+				connObj = DriverManager.getConnection(connUrl + "user=root&password=root");			
+			} catch (Exception exObj) {
+				exObj.printStackTrace();
+			}
+			return connObj;
+	}
+
+
+	/***** Method #3 :: This Method Is Used To Close The Connection With The Database *****/
+	public static void disconnectDb() {
+		try {
+				rsObj.close();
+				stmtObj.close();
+				connObj.close();
+			} catch (SQLException sqlExObj) {
+				sqlExObj.printStackTrace();
+			}		
+	}
+}
+
+
